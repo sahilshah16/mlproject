2b76c9656c29ab9c7ab8eee2ca28f59653db1cd1
sahilshah16 <79090768+sahilshah16@users.noreply.github.com>
2026-01-20 10:39:01 +0000

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,14 @@
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
+from exception import CustomException
+from logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from components.data_transformation import DataTransformation
+from components.data_transformation import DataTransformationConfig
 
 
 @dataclass
@@ -21,7 +24,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv(r'notebooks\data\StudentsPerformance.csv')
+            df=pd.read_csv('notebooks\data\StudentsPerformance.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -35,7 +38,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Ingestion of the data is completed")
+            logging.info("Ingestion of the data is complete")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -47,4 +50,7 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
